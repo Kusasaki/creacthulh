b47b006744947a4780363d6b3bc0740a35840dce
Kusasaki <dianedevi.lacourte@gmail.com>
2022-03-07 23:59:40 +0100

    07/03 Moving between screens

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
 import MyTabs from './components/tabs_navigation/MyTabs'
 
+function FirstScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Bienvenue dans CréaCthulh</Text>
+      <Button
+        title="Accueil"
+        onPress={() => navigation.navigate('Accueil')}
+      />
+    </View>
+  );
+}
+
 const Tab = createMaterialTopTabNavigator();
 
-export default function App() {
+function Accueil({ navigation }) {
   return (
-    <View style={styles.container}>
-      <View style={styles.blanc}></View>
-      <NavigationContainer>
+      <View style={styles.blanc}>
         <MyTabs></MyTabs>
-      </NavigationContainer>
     </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Démarrage">
+        <Stack.Screen name="Démarrage" component={FirstScreen} />
+        <Stack.Screen name="Accueil" component={Accueil} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -26,3 +49,5 @@ const styles = StyleSheet.create({
     height: 50,
   }
 });
+
+export default App;
